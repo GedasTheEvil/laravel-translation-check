074165d52ce6ab6070c49eeb6b286bdb90a5cd51
GedasTheEvil <gediminas.skucas@gmail.com>
2015-05-21 13:08:14 +0300

    Deleted old files

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-VERSION="0.0.1";
-FINAL_NAME="dist/build-${VERSION}.zip";
-
-zip -r ${FINAL_NAME} composer.json example.php src;
-cd dist;
-rm latest.zip;
-ln -s ${FINAL_NAME} latest.zip;
